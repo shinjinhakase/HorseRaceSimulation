aa9a9dcd2d5786a11105a75c7ce657ec298cc8c7
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-05-04 15:16:16 +0900

    騎手のクラスを作成

---
+++ Assets/Scripts/Competitor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Competitor {
+    
+    Horse horse;
+    Jockey jockey;
+
+    public Competitor(Horse horse,Jockey jockey){
+        this.horse = horse;
+        this.jockey = jockey;
+    }
+    
+}
