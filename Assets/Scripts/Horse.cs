a58b89b11f6cc4eff556f8e2a02dce6dc08a440d
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-05-04 12:01:27 +0900

    馬のクラスを作成

---
+++ Assets/Scripts/Horse.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horse {
+
+    private string name;
+    private string sex;
+    private int age;
+    private string father;
+    private string mother;
+    private int speed;
+    private int stamina;
+    private int mental;
+    private int spurt;
+    private Color32 color;
+    
+}
